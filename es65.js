6c21c0ddc1daa619e3a08013432934b09577bf73
Dawa <sherpadawa01@outlook.com>
2022-05-24 22:50:24 +0545

    object and map

---
+++ es65.js
@@ -0,0 +1,24 @@
+//Maps
+
+//map is an iterable;
+// it stores data in ordered fashion;
+
+//stores key value pair;
+//duplicate keys are not allowed like objects;
+
+//DIFFERENCE BETWEEN MAPS AND OBJECTS;
+//objects can only have string or symbol as key;
+//in maps you can use anythings as key like array, number, string
+
+// object literals;
+
+// const person={
+//     1:"dawa", 
+//     age:"24",
+    
+// }
+
+// for (let key in person){
+//     console.log(typeof key)
+// }
+
