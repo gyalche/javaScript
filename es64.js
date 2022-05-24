93365195b0141fffe43f20f1d2bc67e3559e5309
Dawa <sherpadawa01@outlook.com>
2022-05-24 13:43:25 +0545

    sets

--- es64.js
+++ es64.js
@@ -6,18 +6,38 @@
 
 // in set you can add different array with same value;
 
-const numbers = new Set();
-numbers.add(1);
-numbers.add(2);
-numbers.add(3);
-numbers.add(["one", "two", "three"]);
-numbers.add(["one", "two", "three"]);
+// const numbers = new Set();
+// numbers.add(1);
+// numbers.add(2);
+// numbers.add(3);
+// numbers.add(["one", "two", "three"]);
+// numbers.add(["one", "two", "three"]);
 
-// to check if particular value exits or not
-if(numbers.has(10)){
-    console.log("3 is present");
-}else{
-    console.log("It is not present");
+// // to check if particular value exits or not
+// if(numbers.has(10)){
+//     console.log("3 is present");
+// }else{
+//     console.log("It is not present");
+// }
+
+// //since set is iterable we can use for loop;
+// for(let num of numbers){
+//     console.log(num);
+// }
+
+// console.log(numbers);
+
+// REAL CASE USE OF SETS
+// we usually use set when we have unique value;
+// no duplicate value;
+
+const myArray = [1,2,3,4,2,3,4];
+
+const uniqueElement=new Set(myArray);
+
+let length = 0;
+for(let element of uniqueElement){
+    length++;
 }
-
-console.log(numbers);
+console.log(uniqueElement);
+console.log(length);
