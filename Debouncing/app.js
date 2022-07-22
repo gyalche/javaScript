900a52cece8f6c050056936b054ce8a1e66edf3f
Dawa <sherpadawa01@outlook.com>
2022-07-22 10:53:33 +0545

    wored on events like keyup, click, change

---
+++ Debouncing/app.js
@@ -0,0 +1,17 @@
+const myInput = document.getElementById('input-event');
+
+//keyup;
+//input;
+//change;
+
+// myInput.addEventListener('keyup', () => {
+//   console.log(myInput.value);
+// });
+
+myInput.addEventListener('input', () => {
+  console.log(myInput.value);
+});
+
+// myInput.addEventListener('change', () => {
+//   console.log(myInput.value);
+// });
