34388a4334976f913d49b56191321fcd4c257101
Dawa <sherpadawa01@outlook.com>
2022-08-05 14:43:50 +0545

    wokring on block, blockscope, temporal deadzone, illegal schadowing, shadowing

---
+++ NAMASTE_JAVASCRIPT/block_scopt.js
@@ -0,0 +1,13 @@
+// block is dedfinded by curly brackets{}
+// it is used to combine multiple javascript statement into one group;
+
+//Q> why do we need to group all this statement together?
+//=> we need to group all this statement together so that we can use;
+//multiple statement in a place where javascript expects only one statement;
+
+//BLOCK scope;
+{
+    var a=10;
+    let b=20;
+    const c=30;
+}
