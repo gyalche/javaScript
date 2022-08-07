866406292ce8011f028a3ef5b41ad0c42ff84682
Dawa <sherpadawa01@outlook.com>
2022-08-07 11:58:47 +0545

    setTime_Closure learned

--- NAMASTE_JAVASCRIPT/setTime_Closure.js
+++ NAMASTE_JAVASCRIPT/setTime_Closure.js
@@ -1,12 +1,14 @@
 function x() {
-    for(var i = 0; i <= 5; i++)
-
-    function close(x) {
+    for(var i = 0; i <= 5; i++){
+        function close(x) {
         setTimeout(function(){
             console.log(x)
-        },x* 1000)
-    }
+        },x * 1000)
+        }
     close(i)
+    }
+
+    
            
 }
 x();
