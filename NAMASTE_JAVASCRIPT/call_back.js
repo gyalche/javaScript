10628ee6efa44bf0cec4e0bc6740d1b34756d2bd
Dawa <sherpadawa01@outlook.com>
2022-08-14 14:10:42 +0545

    worked on callback function

---
+++ NAMASTE_JAVASCRIPT/call_back.js
@@ -0,0 +1,27 @@
+//what is callback function;
+
+function x(y) {
+    console.log("x")
+    y();
+}
+x(function y(){
+    console.log("y")
+
+})
+
+//how calback is used in a asynchronous ;
+setTimeout(function() {
+    console.log("timer")
+}, 5000);
+
+//crete ac closure;
+function attachEventListeners(){
+    let count=0;
+    document.getElementById("clickMe").
+    addEventListener("click", function xyz(){
+        console.log("Button clicked", count++);
+    })
+}
+attachEventListeners()
+
+//Garbage collection and removeElventlistener;
---
+++ NAMASTE_JAVASCRIPT/function_statement.js
@@ -0,0 +1,43 @@
+//function statement;
+function a(){
+    console.log("function statement")
+}
+
+//function expression;
+var b= function(){
+    console.log("function expression");
+}
+
+//difference between function expression and statement is hoisting;
+
+//function decleration == function statement is also know as function declearation;
+
+//anonymous function == function without a name;
+
+// function(){// it require a function name;
+//     //anonymours function are used in a place where function are used as value;
+// }
+
+//named function expression;
+var a= function abc(){
+
+}
+
+//difference beetween paramter and argumenr;
+var c=function(a, b){//parameter;
+    console.log(a, b)
+}
+c(1,2)//argument;
+
+//the value which we pass inside a function is known as argumnet;
+//the laeble or identifer whcih gets those values are known as paramters;
+
+
+//FIRST CLASS FUNCTION;
+//=> The abality to use function as value is known as first class function;
+
+//First class citizen==first class function;
+
+
+//Arrow functions;
+
