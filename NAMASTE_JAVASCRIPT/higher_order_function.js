e537a3241831dd407d53eea1e7f98fddbd4d06be
Dawa <sherpadawa01@outlook.com>
2022-08-16 13:51:16 +0545

    wokring on higher order function=> function which take an argument as a function

---
+++ NAMASTE_JAVASCRIPT/higher_order_function.js
@@ -0,0 +1,43 @@
+function x() {
+  console.log('call back function');
+}
+function y(x) {
+  // x is a higher order function;
+  x();
+}
+
+// const calculateArea = function (radius) {
+//   const output = [];
+//   for (let i = 0; i < radius.length; i++) {
+//     output.push(Math.PI * radius[i] * radius[i]);
+//   }
+//   return output;
+// };
+
+// console.log(calculateArea(radius));
+
+//doing it in a more efficient way;
+const radius = [1, 2, 3, 4];
+
+const area = function (radius) {
+  return Math.PI * radius * radius;
+};
+
+const circumference = function (radius) {
+  return 2 * Math.PI * radius;
+};
+
+const diamter = function (radius) {
+  return 2 * radius;
+};
+const calculate = function (radius, logic) {
+  const output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(logic(radius[i]));
+  }
+  return output;
+};
+
+console.log(calculate(radius, area));
+console.log(calculate(radius, circumference));
+console.log(calculate(radius, diamter));
