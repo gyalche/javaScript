b5d2cabd6325059477640792188396460f7b5f96
Dawa <sherpadawa01@outlook.com>
2022-08-18 12:32:51 +0545

    worked on filter and reduce, learned a new concept in reduce

---
+++ NAMASTE_JAVASCRIPT/reduce.js
@@ -0,0 +1,57 @@
+const arr = [1, 2, 3, 4, 5];
+
+//max number
+const output = arr.reduce(function (acc, curr) {
+  return acc + curr;
+}, 0);
+
+console.log(output);
+
+//sorting;
+const sorting = arr.sort((a, b) => {
+  return b - a;
+});
+console.log(sorting);
+
+//max;
+function max() {
+  let ans = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > ans) {
+      ans = arr[i];
+    }
+  }
+  return ans;
+}
+console.log(max());
+
+const total = arr.reduce((a, b) => {
+  return a + b;
+}, 0);
+
+console.log('using reduce', total);
+
+const user = [
+  { userName: 'dawa', age: 26 },
+  { userName: 'dawa', age: 75 },
+  { userName: 'dawa', age: 50 },
+  { userName: 'dawa', age: 26 },
+  { userName: 'dawa', age: 75 },
+];
+
+//find=> acc{26:2, 75:1, 50:1}
+const find = user.reduce((acc, curr) => {
+  if (acc[curr.age]) {
+    acc[curr.age] = ++acc[curr.age];
+  } else {
+    acc[curr.age] = 1;
+  }
+  return acc;
+}, {});
+
+console.log(find);
+
+//filter;
+
+const fil = user.filter((x) => x.age < 65).map((m) => m.userName);
+console.log(fil);
--- pt101_closure.js
+++ pt101_closure.js
@@ -1,33 +1,27 @@
-function func(){
-    let counter=0;
-    return function(){
-        if(counter<1){
-            console.log("HI this is your first call to me ")
-            counter++;
-        }else{
-            console.log("I have already been called recently ")
-        }
+function func() {
+  let counter = 0;
+  return function () {
+    if (counter < 1) {
+      console.log('HI this is your first call to me ');
+      counter++;
+    } else {
+      console.log('I have already been called recently ');
     }
+  };
 }
-const myFunc=func();
+const myFunc = func();
 myFunc();
 myFunc();
 
 // close with i;
 function x() {
-    for(var i=1; i<=5; i++){
+  for (var i = 1; i <= 5; i++) {
     function close(x) {
-            
-        setTimeout(function(){
-            console.log(x)
-        },x*1000 )
+      setTimeout(function () {
+        console.log(x);
+      }, x * 1000);
     }
-    close(i)
-    
-    }
+    close(i);
+  }
 }
 x();
-
-
-
-
